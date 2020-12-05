a70ef13f35da0c3992e42a59430dd7ab9bb95c22
jsad20203 <60908174+jsad20203@users.noreply.github.com>
2020-12-04 22:19:35 -0500

    modified SensorActivity.java

--- app/src/main/java/com/example/game0/SensorActivity.java
+++ app/src/main/java/com/example/game0/SensorActivity.java
@@ -13,19 +13,13 @@ public class SensorActivity extends Activity implements SensorEventListener {
     private Sensor mAccelorometer;
     private Sensor mMagneticField;
 
-    /*
-    Pitch: This value represents the angle between a plane
-    parallel to the device's screen and a plane parallel to the ground. Assuming that the bottom
-    edge of the device faces the user and that the screen is face-up, tilting the top edge of the
-    device toward the ground creates a positive pitch angle. The range of values is -π to π.
-    */
+
     /*Roll, This value represents the angle between a plane
     perpendicular to the device's screen and a plane perpendicular to the ground. Assuming that the
      bottom edge of the device faces the user and that the screen is face-up, tilting the left edge
      of the device toward the ground creates a positive roll angle. The range of values is
      -π/2 to π/2.
      */
-    public float pitch;
     public float roll;
 
     private float gravityVector[];
@@ -38,26 +32,15 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mMagneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        mAccelorometer sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccelorometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         // do something if either sensor is unavailable, though they should be available
 
     }
 
     @Override
     public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Do something here if sensor accuracy changes.
-        // if accelerometer changes values
-        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            for(int i = 0; i < 3; i++)
-                gravityVector[i] = event.values[i];
 
-            // if geomagnetic sensor is different
-        } else if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-            for(int i = 0; i < 3; i++)
-                geomagneticVector[i] = event.values[i];
-        }
-
-        this.updatePitchAndRoll();
+        this.updateRoll();
     }
 
     @Override
@@ -74,17 +57,16 @@ public class SensorActivity extends Activity implements SensorEventListener {
                 geomagneticVector[i] = event.values[i];
         }
 
-        this.updatePitchAndRoll();
+        this.updateRoll();
     }
 
-    private void updatePitchAndRoll() {
+    private void updateRoll() {
 
         float R[] = new float[9];
         boolean success = SensorManager.getRotationMatrix(R, null, gravityVector, geomagneticVector);
         if(success) {
             float orientation[] = new float[3];
             SensorManager.getOrientation(R, orientation);
-            pitch = orientation[1];
             roll = orientation[2];
         }
     }
